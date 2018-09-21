7c7198554dffdce4c1c50b028392eb337c61cb50
lspak <ladislav.spak86@gmail.com>
2018-09-21 15:57:16 +0200

    update

--- src/git/test.java
+++ src/git/test.java
@@ -6,9 +6,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+
 
 public class test {
     private WebDriver driver;
@@ -21,19 +26,20 @@ public class test {
 
     @Test
     public void test() throws InterruptedException {
-        driver.get("https://ptk.pixelfederation.com/tool#project/20/env/42/tool/56");
-        driver.findElement(By.id("email")).sendKeys("lspak@pixelfederation.com");
-        driver.findElement(By.id("password")).sendKeys("Pokemon123");
-        driver.findElement(By.id("_submit")).click();
+            driver.get("https://ptk.pixelfederation.com/tool#project/20/env/42/tool/56");
+            driver.findElement(By.id("email")).sendKeys("lspak@pixelfederation.com");
+            driver.findElement(By.id("password")).sendKeys("Pokemon123");
+            driver.findElement(By.id("_submit")).click();
 
-        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.className("l-navigation-togglebutton")));
-        Assert.assertTrue(driver.findElement(By.className("l-navigation-togglebutton")).isDisplayed());
-        driver.findElement(By.className("l-navigation-togglebutton")).click();
+            new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.className("l-navigation-togglebutton")));
+            Assert.assertTrue(driver.findElement(By.className("l-navigation-togglebutton")).isDisplayed());
+            driver.findElement(By.className("l-navigation-togglebutton")).click();
 
-        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.className("l-project-image")));
-        Assert.assertTrue(driver.findElement(By.className("l-project-image")).isDisplayed());
-        driver.findElement(By.)
+            new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.className("l-project-image")));
+            Assert.assertTrue(driver.findElement(By.className("l-project-image")).isDisplayed());
+            driver.findElement(By.xpath("//*[@id=\"lista-container\"]/div/div/div[1]/div[1]/div[1]/div/div/div[1]/div/div/div/div[4]/div[2]/div[1]/a/div")).click();
 
+           
     }
 
     @After
